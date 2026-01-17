4393d021dfb2accc3a21358e48e1567783fe685c
abhijeet8910 <abhidars456@gmail.com>
2026-01-17 14:10:08 +0530

    Updating Card and Projects section layout

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -21,26 +21,44 @@ const cardDetails = [
   <RiTailwindCssFill size={10} className='inline  hover:text-pink-500'/>
   </div>]
 
- }]
+ },
+ {  
+  title: 'Project 3',
+  description: 'Description of project 3',
+  // image: 'https://via.placeholder.com/150',
+  // link: 'https://example.com/project2',
+  icons: [<div>
+  <FaReact size={10} className='inline   hover:text-pink-500'/>
+<RiTailwindCssFill size={10} className='inline  hover:text-pink-500'/>
+</div>]
+
+}]
 
 const Projects = () => {
   return (
-    <div className='h-screen bg-sky-600'>
-      <h1 className='text-6xl text-center  font-mono font-bold mb-10'>Projects</h1>
-      <div className='flex justify-evenly items-center flex-wrap'>
-        {cardDetails.map((card, index) => (
+    <section className=' bg-sky-600'>
+      <h1 className='text-6xl text-center  font-mono font-bold '>Projects</h1>
+   
+    <div className='min-h-screen flex items-center'>
+      
+      <main className='max-w-6xl mx-auto px-12 w-full'>
+        <div className='flex justify-evenly flex-wrap gap-10'>
+            {cardDetails.map((card, index) => (
           <Card
             key={index}
-            title={card.title}
+            title={card.title} 
             description={card.description}
             // image={card.image}
             // link={card.link}
             icons={card.icons}
           />
         ))}
-      </div>
+        </div>
+
+      </main>
       
     </div>
+    </section>
   )
 }
 
--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -11,10 +11,10 @@ type cardProps = {
 
 const Card = ({title, description, image, link, icons}: cardProps) => {
   return (
-    <div className='bg-gray-400 border border-red-300 rounded-xl w-1/5 '>
-    <div className='flex flex-col justify-start items-start'>
-      <h1>{title}</h1>
-      <p>{description}</p>
+    <div className='bg-linear-to-br from-slate-900/90 via-blue-900/90 to-slate-900/90 backdrop-blur-md shadow-2xl rounded-lg w-fit px-6 py-12'>
+    <div className='flex flex-col justify-start items-start gap-4'>
+      <h1 className='text-4xl md:text-5xl lg:text-6xl font-bold'>{title}</h1>
+      <p className=''>{description}</p>
       {/* <img src={image} alt="" />
       <a href={link}>{link}</a> */}
       <div className=''>
